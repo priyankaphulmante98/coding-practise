bccaa780896ed89e29aa6f8cfda6d56ddc12601e
priyanka <priyankaphulmante98@gmail.com>
2022-10-26 20:56:47 +0530

    day-2

---
+++ form-validation/index.js
@@ -0,0 +1,33 @@
+let form = document.getElementById('form')
+form.addEventListener('submit',myfunc);
+
+let registerData=JSON.parse(localStorage.getItem("data"))||[]
+function myfunc(e){
+    e.preventDefault()
+    // console.log('gj')
+   
+    let obj={
+        name:form.name.value,
+        age:form.age.value,
+        des:form.radio.value,
+        priority:form.priority.value,
+        vaccine:form.vaccine.value,
+        uniqueId:Date.now()
+    }
+    const {name,age,des,priority,vaccine}=obj
+
+ if(age<18||age>=40){
+    alert("age should be 18 to 40");
+   
+
+}   
+else{
+
+    
+    registerData.push(obj)
+    localStorage.setItem('data',JSON.stringify(registerData))
+
+    window.location.href="../html/dashboard.html"
+
+}
+}
