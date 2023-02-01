9d63d292132f06275cf22af914457be12cbfa118
unknown <shridharhakke1998@gmail.com>
2023-02-01 19:19:43 +0530

    bubble algorithms

---
+++ DSA-practise-priya/masai_dsa_shree/Sorting_algorithms/bubbleSort.js
@@ -0,0 +1,28 @@
+function BubbleSort(arr){
+let n =  arr.length
+    for(let k=0; k <=n-1; k++){
+
+        for(let j=0; j <=n-1-k; j++){
+
+            if(arr[j] > arr[j+1]){
+                swap(arr,j,j+1)
+            }
+        
+        }
+    }
+console.log(arr)
+    function swap(arr,l,r){
+
+        [arr[l],arr[r]] = [arr[r],arr[l]]
+
+    }
+
+
+
+}
+
+
+
+let arr=[1,2,3,9,8,5,7,4,52,0,-2]
+
+BubbleSort(arr)
