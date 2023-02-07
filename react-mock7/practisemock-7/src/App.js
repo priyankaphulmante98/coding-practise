378183eaaf4e497ab044e5a08a26dcceb97fe93f
unknown <shridharhakke1998@gmail.com>
2023-02-07 13:06:17 +0530

    small ecomm page

--- react-mock7/practisemock-7/src/App.js
+++ react-mock7/practisemock-7/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import EPage from './epage/EPage';
 import AllRoute from './Routes/AllRoute';
 
 function App() {
   return (
     <div className="App">
     
-     <AllRoute />
+     {/* <AllRoute /> */}
+     <EPage/>
     </div>
   );
 }
--- react-mock7/practisemock-7/src/index.js
+++ react-mock7/practisemock-7/src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { ChakraProvider } from '@chakra-ui/react'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
-    </BrowserRouter>
-
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
 );
-
-
